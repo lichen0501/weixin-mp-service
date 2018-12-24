c2b4f397b86dd63ef0c67b1d76d3d1bae14f46e2
lichen <562504047@qq.com>
2018-12-24 19:24:48 +0800

    添加多openid推送支持

--- src/main/java/pers/lichen/weixin/mp/controller/WxTemplateMessageController.java
+++ src/main/java/pers/lichen/weixin/mp/controller/WxTemplateMessageController.java
@@ -7,6 +7,7 @@ import me.chanjar.weixin.mp.bean.template.WxMpTemplateData;
 import me.chanjar.weixin.mp.bean.template.WxMpTemplateMessage;
 import org.springframework.web.bind.annotation.*;
 import pers.lichen.weixin.mp.config.WxMpConfiguration;
+import pers.lichen.weixin.mp.model.params.WxMpTemplateMessages;
 
 import java.util.List;
 
@@ -21,10 +22,28 @@ public class WxTemplateMessageController {
      * @Author: lichen
      * @Date: 2018-12-22 15:39
      */
-    @PostMapping(value = "/send")
-    public void sendTempMessage(@PathVariable String appid, @RequestBody WxMpTemplateMessage data) throws WxErrorException {
+    @RequestMapping(value = "/send",method = RequestMethod.POST)
+    public String sendTempMessage(@PathVariable String appid,@RequestBody WxMpTemplateMessages data) throws WxErrorException {
 
-        WxMpTemplateMessage wxMpTemplateMessage = data;
-        WxMpConfiguration.getMpServices().get(appid).getTemplateMsgService().sendTemplateMsg(wxMpTemplateMessage);
+        String faildUser = "";
+        String error = "";
+        WxMpTemplateMessage wxMpTemplateMessage = data.setSuper();
+        for(String toUser:data.getToUsers()){
+            wxMpTemplateMessage.setToUser(toUser);
+            try {
+                WxMpConfiguration.getMpServices().get(appid).getTemplateMsgService().sendTemplateMsg(wxMpTemplateMessage);
+            }
+            catch (Exception e){
+                faildUser+=(toUser+",");
+                error+=(e.getMessage()+",");
+            }
+        }
+
+        if("".equals(faildUser)){
+            return "消息推送成功";
+        }else {
+            System.out.println(faildUser.substring(0,faildUser.length()-1));
+            return "用户"+faildUser.substring(0,faildUser.length()-1)+"推送失败,错误信息："+error.substring(0,error.length()-1);
+        }
     }
 }
