dea96fd47d4ce366942bcc7708b62b3aae033525
lich <0501>
2019-05-17 15:14:30 +0800

    推送消息功能修复

--- src/main/java/pers/lichen/weixin/mp/controller/WxTemplateMessageController.java
+++ src/main/java/pers/lichen/weixin/mp/controller/WxTemplateMessageController.java
@@ -1,32 +1,29 @@
 package pers.lichen.weixin.mp.controller;
 
 import me.chanjar.weixin.common.error.WxErrorException;
-import me.chanjar.weixin.mp.api.WxMpService;
-import me.chanjar.weixin.mp.api.WxMpTemplateMsgService;
-import me.chanjar.weixin.mp.bean.template.WxMpTemplateData;
 import me.chanjar.weixin.mp.bean.template.WxMpTemplateMessage;
 import org.springframework.web.bind.annotation.*;
 import pers.lichen.weixin.mp.config.WxMpConfiguration;
-import pers.lichen.weixin.mp.model.params.WxMpTemplateMessages;
-
-import java.util.List;
+import pers.lichen.weixin.mp.domain.WxMpTemplateMessages;
 
 @RestController
 @RequestMapping("/wx/templateMsg/{appid}")
 public class WxTemplateMessageController {
 
+
     /**
-     * @Description: 发送模板消息
-     * @Param: [appid, toUser, templateId, url, data]
-     * @return: void
+     * @Description: 批量发送模板消息
+     * @Param: [appid, data]
+     * @return: java.lang.String
      * @Author: lichen
-     * @Date: 2018-12-22 15:39
+     * @Date: 2018-12-27 14:49
      */
-    @RequestMapping(value = "/send",method = RequestMethod.POST)
+    @PostMapping(value = "/send")
     public String sendTempMessage(@PathVariable String appid,@RequestBody WxMpTemplateMessages data) throws WxErrorException {
 
         String faildUser = "";
         String error = "";
+
         WxMpTemplateMessage wxMpTemplateMessage = data.setSuper();
         for(String toUser:data.getToUsers()){
             wxMpTemplateMessage.setToUser(toUser);
